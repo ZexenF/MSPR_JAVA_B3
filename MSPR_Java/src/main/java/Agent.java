52950b23cf3656611f45879e4e89652535f97849
GuardaJeremy <97728835+GuardaJeremy@users.noreply.github.com>
2022-02-26 15:52:30 +0100

    Revert "Merge branch 'dev' of https://github.com/ZexenF/MSPR_JAVA_B3 into dev"
    This reverts commit 790003ec3a91725ff9566210c59981737cfd9916, reversing
    changes made to 93490fc75a644d317e7d7bcde692b152bfa147cc.

---
+++ MSPR_Java/src/main/java/Agent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jeremy
+ */
+public class Agent {
+    public String nom;
+    public String prenom;
+    private String password;
+    public String job;
+
+}
